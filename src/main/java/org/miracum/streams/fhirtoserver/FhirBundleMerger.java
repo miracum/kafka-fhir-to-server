549dc9872cc38172446eee7b6022f4809f8ae333
chgl <chgl@users.noreply.github.com>
2024-11-19 22:16:35 +0100

    feat: support for storing DELETE requests in seperate batch bundles (#123)
    * feat: support for storing DELETE requests in seperate batch bundles
    
    * fix tests
    
    * rm delete

--- src/main/java/org/miracum/streams/fhirtoserver/FhirBundleMerger.java
+++ src/main/java/org/miracum/streams/fhirtoserver/FhirBundleMerger.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.Bundle.HTTPVerb;
 import org.hl7.fhir.r4.model.StringType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,8 @@ public class FhirBundleMerger {
   private static final Logger LOG = LoggerFactory.getLogger(FhirBundleMerger.class);
   private final IFhirPath fhirPath;
 
+  public record MergeResult(Bundle bundle, Bundle deletBundle) {}
+
   public FhirBundleMerger(IFhirPath fhirPath) {
     this.fhirPath = fhirPath;
   }
@@ -27,12 +30,12 @@ public class FhirBundleMerger {
   }
 
   public Bundle merge(List<Bundle> bundles, String fhirPathExpression) {
+    return mergeSeperateDeleteBundles(bundles, fhirPathExpression).bundle();
+  }
+
+  public MergeResult mergeSeperateDeleteBundles(List<Bundle> bundles, String fhirPathExpression) {
     if (bundles.isEmpty()) {
       throw new IllegalArgumentException("bundles parameter cannot be empty");
-    }
-
-    if (bundles.size() == 1) {
-      return bundles.get(0);
     }
 
     // iterate through the list of bundles. The first entry in the list is the
@@ -40,6 +43,8 @@ public class FhirBundleMerger {
     // the most recent unique resource when merging bundles. We use a
     // dictionary to automatically replace older entries with newer ones.
     var setOfUniqueBundleEntries = new HashMap<String, BundleEntryComponent>();
+    var setOfUniqueDeleteBundleEntries = new HashMap<String, BundleEntryComponent>();
+
     for (var bundle : bundles) {
       for (var entryComponent : bundle.getEntry()) {
         var entryIdentifier =
@@ -51,7 +56,12 @@ public class FhirBundleMerger {
               kv("fhirPathExpression", fhirPathExpression),
               kv("bundleId", bundle.getIdElement().toVersionless()));
         } else {
-          setOfUniqueBundleEntries.put(entryIdentifier.get().asStringValue(), entryComponent);
+          if (entryComponent.getRequest().getMethod() == HTTPVerb.DELETE) {
+            setOfUniqueDeleteBundleEntries.put(
+                entryIdentifier.get().asStringValue(), entryComponent);
+          } else {
+            setOfUniqueBundleEntries.put(entryIdentifier.get().asStringValue(), entryComponent);
+          }
         }
       }
     }
@@ -61,12 +71,18 @@ public class FhirBundleMerger {
             .setType(bundles.get(0).getType())
             .setEntry(setOfUniqueBundleEntries.values().stream().toList());
 
-    LOG.debug(
-        "{} input bundles merged to one bundle of {} entries",
-        kv("numInputBundles", bundles.size()),
-        kv("numMergedEntries", resultBundle.getEntry().size()));
+    var deleteBundle =
+        new Bundle()
+            .setType(bundles.get(0).getType())
+            .setEntry(setOfUniqueDeleteBundleEntries.values().stream().toList());
 
-    return resultBundle;
+    LOG.debug(
+        "{} input bundles merged to one bundle of {} entries and one DELETE bundle consisting of {} entries",
+        kv("numInputBundles", bundles.size()),
+        kv("numMergedEntries", resultBundle.getEntry().size()),
+        kv("numMergedDeleteEntries", deleteBundle.getEntry().size()));
+
+    return new MergeResult(resultBundle, deleteBundle);
   }
 
   public List<Bundle> partitionBundle(final Bundle bundle, int maxPartitionSize) {
