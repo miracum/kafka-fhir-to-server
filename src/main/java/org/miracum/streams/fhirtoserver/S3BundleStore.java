91d6d70e57d8fa9b74cb548bf813028ddc05a499
chgl <chgl@users.noreply.github.com>
2025-01-03 20:10:52 +0100

    fix: switch to aws s3 sdk for better ceph compatibility

--- src/main/java/org/miracum/streams/fhirtoserver/S3BundleStore.java
+++ src/main/java/org/miracum/streams/fhirtoserver/S3BundleStore.java
@@ -4,19 +4,8 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.DataFormatException;
 import ca.uhn.fhir.rest.api.Constants;
 import ca.uhn.fhir.util.BundleUtil;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.errors.ErrorResponseException;
-import io.minio.errors.InsufficientDataException;
-import io.minio.errors.InternalException;
-import io.minio.errors.InvalidResponseException;
-import io.minio.errors.ServerException;
-import io.minio.errors.XmlParserException;
-import java.io.ByteArrayInputStream;
+import io.micrometer.common.lang.Nullable;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,51 +15,48 @@ import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Bundle.BundleType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Service
 public class S3BundleStore {
   private static final Logger LOG = LoggerFactory.getLogger(S3BundleStore.class);
-  private MinioClient minioClient;
   private FhirBundleMerger merger;
   private FhirContext fhirContext;
   private FhirBundleMergerConfig mergerConfig;
   private S3Config config;
+  private S3Client s3Client;
 
   public S3BundleStore(
-      @Nullable MinioClient minioClient,
+      @Nullable S3Client s3Client,
       S3Config config,
       FhirBundleMerger merger,
       FhirContext fhirContext,
       FhirBundleMergerConfig mergerConfig) {
-    this.minioClient = minioClient;
+    this.s3Client = s3Client;
     this.config = config;
     this.merger = merger;
     this.fhirContext = fhirContext;
     this.mergerConfig = mergerConfig;
   }
 
-  public Void storeBatch(List<Bundle> bundles)
-      throws DataFormatException,
-          IOException,
-          InvalidKeyException,
-          ErrorResponseException,
-          InsufficientDataException,
-          InternalException,
-          InvalidResponseException,
-          NoSuchAlgorithmException,
-          ServerException,
-          XmlParserException {
-    // start by merging all those bundles
+  public Void storeBatch(List<Bundle> bundles) throws DataFormatException, IOException {
+    // start by merging all those bundles split into POST/PUT and DELETE bundles
     var mergedBundle =
         merger.mergeSeperateDeleteBundles(
             bundles, mergerConfig.entryUniquenessFhirpathExpression());
 
-    // extract all POST/PUT bundle entries
+    // extract all POST/PUT bundle entries (mergedBundle.deleteBundle() contains the DELETE entries)
     var resources = BundleUtil.toListOfResources(fhirContext, mergedBundle.bundle());
 
     var grouped = resources.stream().collect(Collectors.groupingBy(IBaseResource::fhirType));
+
+    LOG.debug(
+        "Storing {} resources in {} buckets ({})",
+        resources.size(),
+        grouped.size(),
+        grouped.keySet());
 
     var parser = fhirContext.newJsonParser();
 
@@ -87,60 +73,75 @@ public class S3BundleStore {
           parser.encodeResourceToWriter(resource, stringWriter);
         }
 
-        var bais =
-            new ByteArrayInputStream(stringWriter.toString().getBytes(StandardCharsets.UTF_8));
         var prefix = config.objectNamePrefix().orElse("");
-        var putArgs =
-            PutObjectArgs.builder()
-                .bucket(config.bucketName())
-                .object(
-                    String.format(
-                        "%s%s/bundle-%s.ndjson",
-                        prefix, resourceType, Instant.now().toEpochMilli()))
-                .stream(bais, bais.available(), 0)
-                .contentType(Constants.CT_FHIR_NDJSON)
-                .build();
 
-        minioClient.putObject(putArgs);
+        var objectKey =
+            String.format(
+                "%s%s/bundle-%s.ndjson", prefix, resourceType, Instant.now().toEpochMilli());
+
+        LOG.debug(
+            "Storing {} resources of type {} as object {}",
+            entry.getValue().size(),
+            entry.getKey(),
+            objectKey);
+
+        var body = RequestBody.fromString(stringWriter.toString());
+        s3Client.putObject(
+            request ->
+                request
+                    .bucket(config.bucketName())
+                    .key(objectKey)
+                    .contentType(Constants.CT_FHIR_NDJSON),
+            body);
       }
     }
+
+    // now, deal with the DELETE entries
 
     // extract the bundles grouped by the resource type of the DELETE request
     var groupedDeletes =
         mergedBundle.deletBundle().getEntry().stream()
             .collect(Collectors.groupingBy(e -> e.getRequest().getUrl().split("/")[0]));
 
+    LOG.debug(
+        "Storing {} delete requests in buckets ({})",
+        groupedDeletes.size(),
+        groupedDeletes.keySet());
+
+    // each entry is one resource type
     for (var entry : groupedDeletes.entrySet()) {
       var deleteBundle = new Bundle();
-      deleteBundle.setType(BundleType.BATCH);
+      deleteBundle.setType(BundleType.TRANSACTION);
 
-      // turns all entries off the merged bundles into a single one
+      // turns all entries of the merged bundles into a single one
       // per resource type
       for (var bundleEntry : entry.getValue()) {
         deleteBundle.addEntry().setRequest(bundleEntry.getRequest());
       }
 
-      try (var stringWriter = new StringBuilderWriter()) {
-        var resourceType = entry.getKey();
+      var resourceType = entry.getKey();
 
-        parser.encodeResourceToWriter(deleteBundle, stringWriter);
+      var deleteBundleJson = parser.encodeResourceToString(deleteBundle);
 
-        var bais =
-            new ByteArrayInputStream(stringWriter.toString().getBytes(StandardCharsets.UTF_8));
-        var prefix = config.objectNamePrefix().orElse("");
-        var putArgs =
-            PutObjectArgs.builder()
-                .bucket(config.bucketName())
-                .object(
-                    String.format(
-                        "%s%s/_delete/bundle-%s.json",
-                        prefix, resourceType, Instant.now().toEpochMilli()))
-                .stream(bais, bais.available(), 0)
-                .contentType(Constants.CT_FHIR_NDJSON)
-                .build();
+      var prefix = config.objectNamePrefix().orElse("");
 
-        minioClient.putObject(putArgs);
-      }
+      var objectKey =
+          String.format(
+              "%s%s/_delete/bundle-%s.json", prefix, resourceType, Instant.now().toEpochMilli());
+
+      LOG.debug(
+          "Storing delete bundle with {} entries as object {}",
+          deleteBundle.getEntry().size(),
+          objectKey);
+
+      var body = RequestBody.fromString(deleteBundleJson);
+      s3Client.putObject(
+          request ->
+              request
+                  .bucket(config.bucketName())
+                  .key(objectKey)
+                  .contentType(Constants.CT_FHIR_JSON_NEW),
+          body);
     }
     return null;
   }
--- src/main/java/org/miracum/streams/fhirtoserver/S3Config.java
+++ src/main/java/org/miracum/streams/fhirtoserver/S3Config.java
@@ -5,13 +5,17 @@ import java.net.URL;
 import java.util.Optional;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
+import software.amazon.awssdk.regions.Region;
 
 @ConfigurationProperties(prefix = "s3")
 @Validated
 public record S3Config(
     boolean enabled,
+    boolean forcePathStyle,
+    long timeoutSeconds,
     URL endpointUrl,
     Optional<String> accessKey,
     Optional<String> secretKey,
     @NotEmpty String bucketName,
+    Region region,
     Optional<String> objectNamePrefix) {}
